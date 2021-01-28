4229b8da14ef2193c1824caefd71901e554bf3c1
Marcio Pancheri <mjpancheri@claretiano.edu.br>
2021-01-28 11:43:06 -0300

    codigo ao final da segunda aula

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
-import styled from 'styled-components'
-import db from '../db.json'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+import db from '../db.json';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizBackground from '../src/components/QuizBackground';
 import QuizLogo from '../src/components/QuizLogo';
 import Widget from '../src/components/Widget';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`;
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `;
 
 // const BackgroundImage = styled.div`
 //   flex: 1;
@@ -30,8 +34,28 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Quiz CSS da Alura</title>
+        <meta name="title" content="Quiz CSS da Alura" />
+        <meta name="description" content="Imersão React/Next.js - Alura" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://aluraquiz.mjpancheri.vercel.app/" />
+        <meta property="og:title" content="Quiz CSS da Alura" />
+        <meta property="og:description" content="Imersão React/Next.js - Alura" />
+        <meta property="og:image" content="" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://aluraquiz.mjpancheri.vercel.app/" />
+        <meta property="twitter:title" content="Quiz CSS da Alura" />
+        <meta property="twitter:description" content="Imersão React/Next.js - Alura" />
+        <meta property="twitter:image" content="" />
+      </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
@@ -39,7 +63,21 @@ export default function Home() {
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>{db.description}</p>
+            <form onSubmit={(event) => {
+              event.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <input
+                onChange={(event) => {
+                  setName(event.target.value);
+                }}
+                placeholder="Digite seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                {`Jogar [${name}]`}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
